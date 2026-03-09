eeafc5b8c759a1022b7659139a1f5de84ff87fb8
irregular6612 <irregular6612@gmail.com>
2026-03-09 11:36:12 +0900

    update recent file system

--- quartz.config.ts
+++ quartz.config.ts
@@ -62,7 +62,7 @@ const config: QuartzConfig = {
     transformers: [
       Plugin.FrontMatter(),
       Plugin.CreatedModifiedDate({
-        priority: ["frontmatter", "git", "filesystem"],
+        priority: ["git", "frontmatter", "filesystem"],
       }),
       Plugin.SyntaxHighlighting({
         theme: {
